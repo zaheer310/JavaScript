73b30daac01db8453cdd317727767a4907717940
zaheer310 <Administrator@DC1.melikshah.com>
2026-06-16 16:52:38 +0530

    Exercise in JavaScript Day_7

---
+++ Day_7/script1.js
@@ -0,0 +1,58 @@
+// // Function Definition and Calling
+// // declaration
+// function sayHello(){
+//     console.log(`Hello World!`)
+// }
+
+// sayHello();
+
+// // Example 1:
+// function greet() {
+// console.log("Welcome to JavaScript!");
+// }
+// greet(); // Output: Welcome to JavaScript!
+// greet(); // Output: Welcome to JavaScript!
+
+// // Example 3: Multiple actions in one function
+
+// function introduction() {
+// console.log("My name is JavaScript");
+// console.log("I was created in 1995");
+// console.log("I run in browsers");
+// }
+// introduction();
+// // Output:
+// // My name is JavaScript
+// // I was created in 1995
+// // I run in browsers
+
+// // Functions With Arguments
+
+// // Example 1:
+// function greetUser(name) {
+//   console.log(`Hello &{name} !`);
+// }
+// greetUser(zaheer);
+
+// Example 2:Calculate average of three numbers
+
+function calcAvgNumb(num1, num2, num3) {
+  console.log((num1 + num2 + num3) / 3);
+}
+
+calcAvgNumb(3, 3, 3);
+
+// Example 4: create personalized messages
+
+function createMessage(name, age) {
+  console.log(`${name} is ${age} years old`);
+}
+
+createMessage(`Zaheer`, 22);
+
+// Example 5: Calculate rectangle area
+function calcArea(length, width) {
+  let area = length * width;
+  console.log(`The area of rectangular field is ${area}`);
+}
+calcArea(30, 12);
